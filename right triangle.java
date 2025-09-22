31471c161ab9440c1d540200e82cdcc7a21c110c
sabari2912 <nishanthsabari48@gmail.com>
2025-09-22 09:26:44 +0530

    right triangle.java

---
+++ right triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class cond1
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for( int row=1;row<=n;row++){
+        for( int col=1;col<=n-row;col++)
+        System.out.print(" ");
+         for( int col=1;col<=row;col++)
+        System.out.print("*");
+        System.out.println();
+        }
+    }
+}
