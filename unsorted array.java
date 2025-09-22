6fe8ac239b4a527f0ef941f5058dfcdf4f60ee4b
sabari2912 <nishanthsabari48@gmail.com>
2025-09-22 09:34:08 +0530

    unsorted array.java

---
+++ unsorted array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int size=sc.nextInt();
+	int num[]=new int[size];
+	for(int ind=0;ind<size;ind++)
+	num[ind]=sc.nextInt();
+	int key=sc.nextInt();
+	int count=0;
+    for(int ind=0;ind<size;ind++)
+    {
+    if(num[ind]<key)
+    count++;
+    }
+    	System.out.print(count);
+}	
+}
