85faa2f0a725a80e8048dff2a482db52c85a6a19
sabari2912 <nishanthsabari48@gmail.com>
2025-09-22 09:29:05 +0530

    floating.java

---
+++ floating.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float inputValue = scanner.nextFloat();
+        System.out.printf("%20.4f\n", inputValue);
+    }
+}
