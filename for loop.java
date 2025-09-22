a0cc5bfc442cea162be67775677f458e518f3213
sabari2912 <nishanthsabari48@gmail.com>
2025-09-22 09:24:50 +0530

    for loop.java

---
+++ for loop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class cond1
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1;i<=n;i++)
+        System.out.print(i+" ");  
+	}
+}
